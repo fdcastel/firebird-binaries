94c4551669a7a3aa71d4a4d25e98237c48a843ec
F.D.Castel <fdcastel@gmail.com>
2023-05-11 00:16:39 -0300

    Adds Rebuild-Databases.ps1.

---
+++ Rebuild-Databases.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+Param(
+    [Parameter(ValueFromPipeline=$true)]
+    [System.String]
+    $DbPath=$Null,
+
+    [System.String]
+    $DbPrefix='test'
+    )
+
+$dbFolder = Join-Path $env:TEMP 'firebird-databases'
+if ($DbPath) {
+    $dbFolder = $DbPath
+}
+
+mkdir -Force $dbFolder | Out-Null
+
+# Build databases
+'fb25', 'fb30', 'fb40', 'fb50' | ForEach-Object {
+    $engine = $_
+
+    $engineFolder = Join-Path $PSScriptRoot $engine
+    $isql = Join-Path $engineFolder 'isql.exe'
+
+    $database = Join-Path $dbFolder "$DbPrefix.$engine.fdb"
+    Remove-Item $database -Force -ErrorAction SilentlyContinue
+
+    Write-Verbose "Creating '$engine' database in '$database'..."
+    @"
+    CREATE DATABASE '$database'
+        USER 'SYSDBA' PASSWORD 'masterkey'
+        PAGE_SIZE 8192 DEFAULT CHARACTER SET UTF8;
+"@ | & $isql -quiet | Out-Null
+}
